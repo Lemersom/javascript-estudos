eaf59d72e1beb5a8e3743e0bafb087b53ae3caad
Lemersom <lemerfilho@gmail.com>
2023-04-13 22:43:06 -0300

    add express-node

---
+++ express-node/aula001/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const app = express() //server      usar: npm init  npm install  npm start
+
+app.use((req, res, next) =>{
+    console.log("Houve uma requisição")
+    next()
+})
+
+app.use(express.static("public")) //definindo a pasta que será publica
+
+app.get("/", (req, res) => {
+    res.end("<h1>Teste Express</h1>") //usar res.end no fim das rotas para finalizar a requisição
+})
+
+app.get("/usuario/:user/mostrar", (req, res) =>{
+    res.end("<h1>Tela Nome: " + req.params.user + "</h1>")
+})
+
+app.listen(3000, () => {
+    console.log("Running on por 3000")
+})

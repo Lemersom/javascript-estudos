810a2e0de871058144e5b2c43a1ad406a20148ce
Lémersom <lemerfilho@gmail.com>
2023-06-16 00:08:40 -0300

    add: ex-mongo

--- express-node/projeto/helpers/mysql.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize")
-
-const sequelize = new Sequelize("name", "user", "password", {host: "localhost", dialect: "mysql"})
-
-sequelize.authenticate()
-    .then(() => console.log("Conectado ao Mysql"))
-    .catch(error => console.log(error))
-
-module.exports = sequelize
